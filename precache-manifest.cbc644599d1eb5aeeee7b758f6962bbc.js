aa5b81e8f2c31ef931e2822919c5ca7314cee893
Abraham Mamani <abraham.e.mamani@gmail.com>
2019-08-29 13:58:43 -0300

    Updates

---
+++ precache-manifest.cbc644599d1eb5aeeee7b758f6962bbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0768b1d1e082c689b3253071b3731f3a",
+    "url": "/vscode/index.html"
+  },
+  {
+    "revision": "b81a36c4a4faffdd0c4b",
+    "url": "/vscode/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "00ae7373a74ff097184a",
+    "url": "/vscode/static/css/main.54877e2d.chunk.css"
+  },
+  {
+    "revision": "b81a36c4a4faffdd0c4b",
+    "url": "/vscode/static/js/2.d782326d.chunk.js"
+  },
+  {
+    "revision": "00ae7373a74ff097184a",
+    "url": "/vscode/static/js/main.57290459.chunk.js"
+  },
+  {
+    "revision": "458b93b9d73c013be0b4",
+    "url": "/vscode/static/js/runtime~main.e2dad7a5.js"
+  },
+  {
+    "revision": "f7ac676c97d687cf89cf9934886463f0",
+    "url": "/vscode/static/media/Backend.f7ac676c.png"
+  },
+  {
+    "revision": "003d739f6c81786470886f32b3cd2d37",
+    "url": "/vscode/static/media/Frontend.003d739f.png"
+  }
+]);
